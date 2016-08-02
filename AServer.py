b60178e140fb365163708181c669fe52ad1c640a
li-robot <njcitrobot@yahoo.com>
2016-08-02 19:03:00 +0800

    add some feature

--- AServer.py
+++ AServer.py
@@ -1,17 +1,36 @@
 import socket, threading, time, queue, json, traceback
 
 msgQueue = queue.Queue()
+DEBUG_MODE = True
+
 
 class User:
     nickName = ''
     userId = ''
-    
+
+
 class Message:
     requestId = ''
     requestType = ''
     requestContent = ''
     forward = ''
 
+
+class ErrorCode:
+    REQUEST_SUCCESS = 0
+    REQUEST_FORMAT_BAD = 1
+    REQUEST_FAILED = 2
+
+
+
+class Response:
+    
+    def response(requestId, errorCode):
+        ret = json.loads('{}')
+        ret['requestId'] = requestId
+        ret['errorCode'] = errorCode
+        return json.dumps(ret) + '\n'
+        
 
 class RequestClient(threading.Thread):
 
@@ -27,22 +46,43 @@ class RequestClient(threading.Thread):
         while self.runFlag:
             try:
                 data = self.socket.recv(1024)
-                dataJson = json.loads(data.decode('utf-8').strip())
                 
-                
-                msg = Message()
-                msg.requestId = dataJson['requestId']
-                msg.requestType = dataJson['requestType']
-                msgQueue.put(msg)
-
                 print(data.decode('utf-8').strip())
+                dataJson = json.loads(data.decode('utf-8').strip())
+
+                if 'message' == dataJson['requestType']:
+                    msg = parseMessage(dataJson)
+                    msgQueue.put(msg)
+                    resp = Response.response(self.requestId, ErrorCode.REQUEST_SUCCESS)
+                    self.writeData(resp)
+
+                if 'login' == dataJson['requestType']:
+                    pass
                 
             except:
                 self.runFlag = False
-                traceback.print_exc()
+                resp = Response.response(self.requestId, ErrorCode.REQUEST_FORMAT_BAD)
+                self.writeData(resp)
+                if DEBUG_MODE:
+                    traceback.print_exc()
 
     def writeData(self, data):
-            self.socket.sendall(data)
+        if self.socket:
+            self.socket.sendall(data.encode('utf-8'))
+
+    def parseMessage(dataJson):
+        msg = Message()
+        msg.requestId = dataJson['requestId']
+        msg.requestType = dataJson['requestType']
+        msg.requestContent = dataJson['requestContent']
+        msg.forward = dataJson['forward']
+        return msg
+
+    def handleLogin(dataJson):
+        pass
+
+    def verifyUser(dataJson):
+        pass
 
 
 
@@ -56,14 +96,20 @@ class PollThread(threading.Thread):
 
     def run(self):
         while self.runFlag:
-            for item in self.clientList:
-                try:
-                    item.writeData('response'.encode())
-                except:
-                    self.clientList.remove(item)
-                
-            time.sleep(3)
-    
+           msg = msgQueue.get()
+           print(" == get a message == ")
+           print(" == message id == " + msg.requestId)
+
+    def handleMessage(self, message):
+        if message.requestType == 'message':
+            pass
+
+    def getClientByUserId(self, userId):
+        for client in self.clientList:
+            if client.user.userId == userId:
+                return client
+        return None
+
 
 if __name__ == '__main__':
     
